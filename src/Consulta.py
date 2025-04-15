eddfdacc1ab885f131b22f8373f4eb6c217a1197
jeanliz <jeanliz@alunos.utfpr.edu.br>
2025-04-15 20:18:03 -0300

    Create  class Consulta e Pessoa

---
+++ src/Consulta.py
@@ -0,0 +1,11 @@
+from functools import partial
+import pyecore.ecore as Ecore
+from pyecore.ecore import *
+from types import String, Integer
+
+
+name = 'Diagrama_Classe_Clinica_Odontologica_v2'
+nsURI = 'http:///Diagrama_Classe_Clinica_Odontologica_v2.ecore'
+nsPrefix = 'Diagrama_Classe_Clinica_Odontologica_v2'
+
+eClass = EPackage(name=name, nsURI=nsURI, nsPrefix=nsPrefix)
