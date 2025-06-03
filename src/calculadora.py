4a0048618ae37b6449870a7520980de8e587864f
Lucas Furtado Souza <lucasfurtado256@gmail.com>
2025-06-03 20:20:35 -0300

    Create calculadora.py

---
+++ src/calculadora.py
@@ -0,0 +1,12 @@
+class Calculadora:
+    def soma(num1,num2):
+        return num1 + num2
+    
+    def subtracao(num1, num2):
+        pass
+
+    def multiplicacao(num1, num2):
+        pass
+
+    
+    
